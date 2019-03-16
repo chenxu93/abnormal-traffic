992dca3b46864fe14aa563637b8573fa4a4440be
chenxu93 <42310082+chenxu93@users.noreply.github.com>
2019-03-16 17:43:16 +0800

    Update cnn_model_11.py

--- code_classify/cnn_model_11.py
+++ code_classify/cnn_model_11.py
@@ -11,7 +11,7 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 #============================================================================
-#读取数据
+
 
 def data_prepare(f1_name,f2_name,y1,y2):
 	d1 = f1_name.values
@@ -21,7 +21,7 @@ def data_prepare(f1_name,f2_name,y1,y2):
 
 	dataset = np.concatenate((d1,d2),axis=0)
 
-	#打乱
+	
 	np.random.shuffle(dataset)
 	return dataset
 
@@ -72,10 +72,6 @@ PortScan_2 = pd.read_csv("../flow_labeled/labeld_PortScan_2.csv")#158329  > 1586
 
 DDoS = pd.read_csv("../flow_labeled/labeld_DDoS.csv")#16050
 
-#由于Heartbleed和Infiltraton攻击非常少，在做多分类的时候，并不考虑这两类攻击
-#多分类 做11分类 正常+10类攻击
-
-#二分类可考虑Heartbleed和Infiltraton攻击
 
 print("\ndataset prepared,cost time:%d" %(time.time() - start))
 
@@ -101,7 +97,7 @@ d13 = data2feature(DDoS,10)
 Data_tupple = (d0,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13)
 
 Data = np.concatenate(Data_tupple,axis=0)
-#是否丢弃五元组信息
+
 Data = discard_fiv_tupple(Data)
 
 np.random.shuffle(Data)
@@ -112,12 +108,12 @@ y_raw = np.array(Data[:,-1],dtype="int32")
 
 data_train,data_test,label_train,label_test = train_test_split(x_raw,y_raw,test_size=0.2,random_state=0)
 #==========================================================================
-#准备要可视化的数据
+
 
 
 #==========================================================================
 def labels_transform(mlist,classes):
-	#把一个一维的标签list转化为一个 shape为[batch_size,classes]的numpy数组
+	
 	batch_label = np.zeros((len(mlist),classes),dtype="i4")
 	for i in range(len(mlist)):
 		batch_label[i][mlist[i]] = 1
@@ -180,31 +176,8 @@ b_fc2 = bias_variable([classes_num])
 logits = tf.matmul(cnn_fc1_drop,W_fc2) + b_fc2
 
 
-'''
-# LSTM network
-lstm_input = tf.reshape(cnn_fc1_drop,[-1,lstm_timestep_size,lstm_input_size])
-
-lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_units=lstm_hidden_layers,forget_bias=1.0,
-	state_is_tuple=True,activation=None)
-
-rnn_layers = [tf.nn.rnn_cell.LSTMCell(size) for size in [256,256]]
-
-multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
-
-init_state = multi_rnn_cell.zero_state(batch_size,dtype=tf.float32)
-
-outputs,state = tf.nn.dynamic_rnn(cell=multi_rnn_cell,inputs=lstm_input,
-	initial_state=init_state,dtype=tf.float32,time_major=False)
-
-h_state = state[-1][1] 
-
-W_lstm = tf.Variable(tf.truncated_normal(shape=[256,classes_num],stddev=0.1),dtype=tf.float32)
-bias_lstm = tf.Variable(tf.constant(0.15,dtype=tf.float32,shape=[classes_num]))
-logits = tf.matmul(h_state,W_lstm) + bias_lstm
-'''
 
 
-# 损失和评估函数
 
 predictions = {
 	"classes":tf.argmax(input=logits,axis=1),
@@ -221,7 +194,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 
 
-#下面这四个指标是 local variable 需要在session 里面单独初始化，否则会报错
+
 TP = tf.metrics.true_positives(labels=tf.argmax(y,axis=1),predictions=predictions["classes"])
 FP = tf.metrics.false_positives(labels=tf.argmax(y,axis=1),predictions=predictions["classes"])
 TN = tf.metrics.true_negatives(labels=tf.argmax(y,axis=1),predictions=predictions["classes"])
@@ -230,17 +203,17 @@ recall = tf.metrics.recall(labels=tf.argmax(y,axis=1),predictions=predictions["c
 tf_accuracy = tf.metrics.accuracy(labels=tf.argmax(y,axis=1),predictions=predictions["classes"])
 
 
-#设置GPU按需增长
+
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.7
 config.gpu_options.allow_growth = True
 
 sess = tf.Session()
 
-# 开始训练和测试
+
 print("\n"+"="*50 +"Benign Trainging"+"="*50)
 sess.run(tf.global_variables_initializer())
-sess.run(tf.local_variables_initializer())#初始化局部变量
+sess.run(tf.local_variables_initializer())
 _batch_size = 128
 mydata_train = DataSet(data_train,label_train)
 statr = time.time()
@@ -251,15 +224,15 @@ for i in range(train_iter):
 
 		train_accuracy = sess.run(accuracy,feed_dict={_X:batch[0],y:labels,
 			keep_prob:1.0,batch_size:_batch_size})
-		#已经迭代完成的 epoch 数：
+		
 		print("\nthe %dth loop,training accuracy:%f" %(i+1,train_accuracy))
 	sess.run(train_op,feed_dict={_X:batch[0],y:labels,keep_prob:0.5,
 		batch_size:_batch_size})
 
 print("\ntraining finished cost time:%f" %(time.time() - statr))
-#计算测试数据的准确率
 
-#批量测试：
+
+
 test_accuracy = 0
 true_positives = 0
 false_positives = 0
